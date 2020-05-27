8a8a61ac4cef2c40894205eb2b97d826312b0045
richardbobo1 <richard.bobo@gmail.com>
2020-05-27 12:58:38 -0400

    added create and delete to controllers

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -9,5 +9,22 @@ class PropertiesController < ApplicationController
         property = Property.find(params[:id])
         render json: property.as_json(:include => [:owner, :reviews])
     end
+
+    def create
+        property = Property.create(property_params)
+        render json: property, include: [:owner]
+    end 
+
+    def destroy
+        property = Property.find_by(id: params[:id])
+        property.destroy
+        render json: property 
+    end 
+
+    private
+
+    def property_params
+        params.require(:property).permit(:owner_id, :name, :city, :state, :details, :img_url, :price_per_night)
+    end
     
 end
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -11,7 +11,20 @@ class ReservationsController < ApplicationController
     end
 
     def create
-    
+        reservation = Reservation.create(reservation_params)
+        render json: property, include: [:user, :property]
+    end 
+
+    def destroy
+        reservation = Reservation.find_by(id: params[:id])
+        reservation.destroy
+        render json: reservation 
+    end 
+
+    private
+
+    def reservation_params
+        params.require(:reservation).permit(:user_id, :property_id, :start_date, :end_date)
     end
 
     
